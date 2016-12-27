4f3ba3d52c5218dbe3f0caed15e663f5cb0479d5
Jonathan Piron <jonathanpiron@gmail.com>
2016-12-27 12:11:55 +0100

    Add support for a root path

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# remove trailing / if any.
+SELF_URL_PATH=${SELF_URL_PATH/%\//}
+
+# extract the root path from SELF_URL_PATH (i.e http://domain.tld/<root_path>).
+ROOT_PATH=${SELF_URL_PATH/#http*\:\/\/*\//}
+if [ "${ROOT_PATH}" == "${SELF_URL_PATH}" ]; then
+    # no root path in SELF_URL_PATH.
+    mkdir -p /var/tmp
+    ln -s "/var/www" "/var/tmp/www"
+else
+    mkdir -p /var/tmp/www
+    ln -s "/var/www" "/var/tmp/www/${ROOT_PATH}"
+fi
+
+php /configure-db.php
+exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
